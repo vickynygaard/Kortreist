d8e44cd2833b84322eb65231059b34004d556952
vickynygaard <vanygaard@uia.no>
2025-03-12 13:35:56 +0100

    endringer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
 			defaultTheme='system'
 			disableTransitionOnChange
 		>
+
+
+			<Navbar />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	)
