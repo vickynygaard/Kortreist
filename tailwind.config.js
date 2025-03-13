be8d3fb0d688a1734ba5be9c2fb7939d46aa6f5c
vickynygaard <vanygaard@uia.no>
2025-03-13 11:37:27 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
 	theme: {
 		extend: {
 		  colors: {
-			customBlue: '#1D4ED8', //Blå outline farge #5573CE
+			customBlue: '#1D4ED8', //Blå farge #5573CE
+			customViolet: '#311687', //Mørk lilla farge #311687
+			customYellow: '#FFF8DA', //Lysegul farge #FFF8DA
 		  },
 		},
 	  },
