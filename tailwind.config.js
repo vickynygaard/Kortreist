ba3a03b91fbe236afcf49a272b5e4615b86bc777
vickynygaard <vanygaard@uia.no>
2025-03-13 11:31:24 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	darkMode: 'class',
+	theme: {
+		extend: {
+		  colors: {
+			customBlue: '#1D4ED8', //Blå outline farge #5573CE
+		  },
+		},
+	  },
 	plugins: [require('tailwindcss-safe-area')],
 }
