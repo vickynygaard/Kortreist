f0788c29a6cd17ff192c5d0e124f1f2d84c94002
vickynygaard <vanygaard@uia.no>
2025-03-17 12:15:05 +0100

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ const NavItem = ({ href, icon }: { href: string; icon: React.ReactNode }) => {
       {/* Sirkel bak aktivt ikon i Navbar*/}
       {isActive && <span className="absolute w-10 h-10 bg-violet-400/40 rounded-full -z-10"></span>} 
       
-      <span className={`${isActive ? "text-violet-950" : "text-gray-500"} hover:text-white transition-colors`}>
+      <span className={`${isActive ? "text-violet-950" : "text-gray-500"}`}>
         {icon}
       </span>
     </Link>
@@ -19,11 +19,11 @@ const NavItem = ({ href, icon }: { href: string; icon: React.ReactNode }) => {
 }
 
 const Navbar = () => (
-  <nav className="bg-purple-200 fixed bottom-0 left-0 w-full h-14 p-4 flex justify-around rounded-t-lg">
+  <nav className="bg-purple-200 fixed bottom-0 left-0 w-full h-14 p-2 flex justify-around rounded-t-lg">
   <NavItem href="/" icon={<Home size={24} className="text-icon" />} />
-  <NavItem href="/team/team" icon={<Users size={24} className="text-icon" />} />
-  <NavItem href="/leaderboard/leaderboard" icon={<Trophy size={24} className="text-icon" />} />
-  <NavItem href="/profile/profile" icon={<User size={24} className="text-icon" />} />
+  <NavItem href="/team" icon={<Users size={24} className="text-icon" />} />
+  <NavItem href="/leaderboard" icon={<Trophy size={24} className="text-icon" />} />
+  <NavItem href="/profile" icon={<User size={24} className="text-icon" />} />
 </nav>
 );
 
