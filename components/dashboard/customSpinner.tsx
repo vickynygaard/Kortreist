8e455fb5d419e81b560bc2593b3976e5645d6952
jonaslefdal <jonas.lefdal@gmail.com>
2025-03-31 06:49:42 +0200

    spinnerBG

--- components/dashboard/customSpinner.tsx
+++ components/dashboard/customSpinner.tsx
@@ -1,6 +1,6 @@
 export default function CustomSpinner() {
     return (
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="fixed inset-0 flex items-center justify-center z-50">
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
       </div>
     );
