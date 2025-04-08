b51754a6123b3e36e5c331a89c3ca65c37463c80
vickynygaard <vanygaard@uia.no>
2025-04-08 13:22:46 +0200

    Dashboard meny update

--- components/buttons/dashboardButton.tsx
+++ components/buttons/dashboardButton.tsx
@@ -15,22 +15,29 @@ interface ButtonProps {
     return (
       <button 
         onClick={() => router.push(href)} 
-        className="grid grid-cols-5 grid-rows-2 h-24 w-full rounded-2xl bg-customYellow2 border-2 border-violet-900 gap-2 
+        className="grid grid-cols-5 h-24 w-full rounded-2xl bg-customYellow2 border-2 border-violet-900
                 focus:ring focus:ring-violet-600"
       >
       
-      {/* Grid for Ikon-Tittel-Beskrivelse */}
-      <div className="col-span-4 row-span-2 grid grid-cols-3 grid-rows-4 py-2">
-        <div className="row-span-4 flex items-center justify-center"><Image src={image} alt={title} width={64} height={64} /></div>
-        <div className="col-span-2 row-span-2 flex items-center justify-start text-xl font-bold truncate">{title}</div>
-        <div className="col-span-2 row-span-2 flex items-center justify-start text-sm text-left">{description}</div>
-      </div>
+        {/*Ikon*/}
+        <div className="col-span-1 px-3 flex items-center justify-center">
+          <Image src={image} alt={title} width={64} height={64} />
+        </div>
 
-      {/* Pil-ikon */}
-      {/* Arrow Icon */}
-      <div className="row-span-2 flex items-center justify-center self-center justify-self-center">
-          <ArrowRight size={24} strokeWidth={2} />
-      </div>
+        {/*Tittel og beskrivelse */}
+        <div className="col-span-3 flex flex-col justify-center text-left space-y-1">
+          <span className="text-xl font-bold truncate py-0.5 rounded">
+            {title}
+          </span>
+          <span className="text-sm py-0.5 rounded">
+            {description}
+          </span>
+        </div>
+
+        {/* Pil-ikon */}
+        <div className="col-span-1 flex items-center justify-center">
+            <ArrowRight size={24} strokeWidth={2} />
+        </div>
     </button>
 
     );
